3d3e8440d3a39af590fe8171bcc7a4818cdfccbb
Ricardo Izaguirre <A18107796@idat.edu.pe>
2021-05-28 22:48:16 -0500

    Agregue la clase usuario para continuar con el login

---
+++ src/main/java/com/educacionperu21/apirest/entities/Usuario.java
@@ -0,0 +1,32 @@
+package com.educacionperu21.apirest.entities;
+
+public class Usuario {
+	private Integer idUsuario;
+	private String email;
+	private String password;
+
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
