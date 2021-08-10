ecaa42653d72e48b8673d7b1cd64366f385f9c59
Ricardo <A18107796@idat.edu.pe>
2021-08-10 00:28:42 -0500

    Cambios

--- src/main/java/com/educacionperu21/apirest/dao/MatriculasDAO.java
+++ src/main/java/com/educacionperu21/apirest/dao/MatriculasDAO.java
@@ -13,20 +13,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 public interface MatriculasDAO extends GenericJPAStatusRepository<Matricula, Integer> {
 
-	@Query(value = "SELECT * FROM" + " matriculas" + " where " + "matriculas.id_periodo = "
-			+ "( select periodos.id from periodos where periodos.fecha_inicio = (select max(periodos.fecha_inicio) from periodos))"
-			+ "and matriculas.id_estudiante = ?1", nativeQuery = true)
-	Optional<Matricula> findMostRecentMatriculaById(Integer id);
+    @Query(value = "SELECT * FROM" + " matriculas" + " where " + "matriculas.id_periodo = "
+            + "( select periodos.id from periodos where periodos.fecha_inicio = (select max(periodos.fecha_inicio) from periodos))"
+            + "and matriculas.id_estudiante = ?1", nativeQuery = true)
+    Optional<Matricula> findMostRecentMatriculaById(Integer id);
 
-	@Query(value = "CALL estudiante_matriculado(:idStudent);", nativeQuery = true)
-	Optional<Matricula> studentMatriculado(Integer idStudent);
+    @Query(value = "CALL estudiante_matriculado(:idStudent);", nativeQuery = true)
+    Optional<Matricula> studentMatriculado(Integer idStudent);
 
-	@Query(value = "select m from Matricula m where m.estudiante.num_doc = ?1")
-	List<Matricula> getMatriculaByEstudiante(String dni);
+    @Query(value = "select m from Matricula m where m.estudiante.num_doc = ?1")
+    List<Matricula> getMatriculaByEstudiante(String dni);
 
-	@Transactional
-	@Modifying
-	@Query("update Matricula m set m.estado = ?1 where m.id = ?2")
-	int updateEstado(Estado estado, Integer id);
+    @Transactional
+    @Modifying
+    @Query("update Matricula m set m.estado = ?1 where m.id = ?2")
+    int updateEstado(Estado estado, Integer id);
 
 }
